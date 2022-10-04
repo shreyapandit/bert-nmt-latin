1fcd021368fc292de78cf580da89761fa14e1d21
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 23:32:44 +0000

    Adding numeric_owner as keyword arguement

--- fairseq/file_utils.py
+++ fairseq/file_utils.py
@@ -121,7 +121,7 @@ def load_archive_file(name_or_path):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(archive, tempdir)
